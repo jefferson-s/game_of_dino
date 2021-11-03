0b08146d8e9f50f08192cffc94dfe2bf7ae22d71
jefferson-s <jefferson.santos10@outlook.com>
2021-11-03 11:17:52 -0300

    function for create cactus

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //query selects the entire page and selects dino
 
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
 
 //adding events to keyboard buttons to control the dino
@@ -42,6 +43,16 @@ function jump() {
 }
 
 // funçtion for create cactus
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
 
 
